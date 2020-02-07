0c7c14e60f6c38d6db770761f38ca9caeab5a0e5
Nathan Maurice <Nathan.maurice8@etu.univ-lorraine.fr>
2020-02-07 14:02:48 +0100

    etape 4

--- projet-monster/projet-monster/Module/actions.js
+++ projet-monster/projet-monster/Module/actions.js
@@ -1,5 +1,5 @@
 
-
+    let fnc;
     let name ;
     let life ;
     let money;
@@ -19,18 +19,21 @@
     }
 
     export function run(){
+        fnc =1;
         if(awake == false && life > 0) {
             life = life - 1;
         }
     }
 
     export function fight(){
+        fnc=2;
         if(awake === false && life > 3) {
             life = life - 3;
         }
     }
 
     export function work(){
+        fnc =3;
         if(awake === false && life > 0) {
             life = life - 1;
             money = money + 2;
@@ -38,6 +41,7 @@
     }
 
     export function eat(){
+        fnc=4;
         if(awake === false && life > 0 && money > 3) {
             money = money - 3;
             life = life + 2;
@@ -46,9 +50,63 @@
 
 
     export function sleep(){
-
+        fnc=5;
         awake = true;
         setTimeout(() => {
             awake = false;
             life++; }, 10000);
+    }
+
+    export function kill(){
+        life = 0;
+        money = 0;
+        name = '';
+        awake = false;
+    }
+
+    export function newlife(){
+        init('flo', 20 , 10, false );
+    }
+
+
+    export function automatic(){
+        life--;
+        let etp =   Math.floor(Math.random() * (5 - 1 +1)) + 1;
+        if(etp==1){
+            run();
+        }
+        if (etp==2){
+            fight();
+        }
+        if (etp==3){
+            work();
+        }
+        if (etp==4){
+           eat();
+        }
+        if (etp==5){
+            sleep();
+        }
+    }
+
+
+
+    export function showStatus(){
+    let tl =0;
+    let elt = document.getElementById('monster');
+    if(life < 5){
+        elt.style.backgroundColor = "red";
+    }
+    if(life > 5 && life < 10){
+        elt.style.backgroundColor  = "orange";
+    }
+    if(life > 10 && life < 15 ){
+        elt.style.backgroundColor = 'blue';
+    }
+    if(life > 15){
+        elt.style.backgroundColor ='lightgreen';
+    }
+    tl = money/3+"px";
+    console.log(tl);
+    elt.style.borderWidth = tl;
     }
--- projet-monster/projet-monster/Module/app.js
+++ projet-monster/projet-monster/Module/app.js
@@ -9,6 +9,8 @@ let beat = document.querySelector("#b5");
 let bshow = document.querySelector("#b6");
 let bkill = document.querySelector("#k");
 let bnewlife = document.querySelector("#b1");
+let bauto = document.querySelector("#b8");
+let auto = false;
 
 export function start(){
     act.init('flo', 20 , 10, false );
@@ -19,7 +21,7 @@ export function start(){
 window.addEventListener("load", () => {
     bshow.addEventListener("click", (event) => {
         aff.displayStatus();
-        alert(act.get());
+        alert("nom du monstre : " +act.get()[0]+'\n'+"pv du monstre : "+act.get()[1]+'\n'+"L'argent du monstre : "+ act.get()[2]);
     });
 });
 
@@ -27,6 +29,7 @@ window.addEventListener("load", () => {
     brun.addEventListener("click", (event) => {
         if(act.get()[3] == false && act.get()[1] > 0) {
             act.run();
+            act.showStatus();
             aff.log('le monstre court : -1 life');
             aff.displayStatus();
         }
@@ -37,6 +40,7 @@ window.addEventListener("load", () => {
     bfight.addEventListener("click", (event) => {
         if(act.get()[3] == false && act.get()[1] > 3) {
             act.fight();
+            act.showStatus();
             aff.log('le monstre se bat : -3 life');
             aff.displayStatus();
         }
@@ -47,6 +51,7 @@ window.addEventListener("load", () => {
         bwork.addEventListener("click", (event) => {
             if(act.get()[3] == false && act.get()[1] > 0 ) {
                 act.work();
+                act.showStatus();
                 aff.log('le monstre tavail : -1 life, +2 money');
                 aff.displayStatus();
             }
@@ -57,6 +62,7 @@ window.addEventListener("load", () => {
     beat.addEventListener("click", (event) => {
         if(act.get()[3] == false && act.get()[1] > 0 && act.get()[2] >3) {
             act.eat();
+            act.showStatus();
             aff.log('le monstre mange : +2 life, -3 money');
             aff.displayStatus();
         }
@@ -67,11 +73,52 @@ window.addEventListener("load", () => {
     bsleep.addEventListener("click", (event) => {
         if(act.get()[3] == false && act.get()[1] > 0) {
             act.sleep();
+            act.showStatus();
             aff.log('le monstre dort ');
             aff.displayStatus();
             setTimeout(() => {aff.log('le monstre se réveil');aff.displayStatus(); }, 10000);
 
 
         }
+    });
+});
+
+window.addEventListener("load", () => {
+    bkill.addEventListener("click", (event) => {
+        if(act.get()[3] == false && act.get()[1] > 0 && act.get()[2] >3) {
+            act.kill();
+            act.showStatus();
+            aff.log('le monstre est mort');
+            aff.displayStatus();
+        }
+    });
+});
+
+window.addEventListener("load", () => {
+    bnewlife.addEventListener("click", (event) => {
+        act.newlife();
+        act.showStatus();
+        aff.log('le monstre nait');
+            aff.displayStatus();
+    });
+});
+
+let interval =0;
+window.addEventListener("load", () => {
+    bauto.addEventListener("click", (event) => {
+        if(auto == false) {
+            auto=true;
+            aff.log('Le monstre est en automatique');
+            aff.displayStatus();
+           inteval = setInterval(() => {
+                act.automatic();
+               act.showStatus();
+               aff.displayStatus();
+            }, 12000);
+        }
+        //else{
+         //   auto= false;
+        //    clearInterval(interval);
+        //    aff.log("Le monstre n'est plus en automatique");}
     });
 });
